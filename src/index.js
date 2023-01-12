6ffd8b406bb9f4f6013dc47f6147cf3b172ca877
Anastasiia Tarasova <muursea@gmail.com>
2023-01-12 21:22:03 +0300

    docs: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    if(n >= 0) {
+        return +n.toString().split('').reverse().join('') 
+      } else{
+        return +(n * -1).toString().split('').reverse().join('');
+      }
 }
